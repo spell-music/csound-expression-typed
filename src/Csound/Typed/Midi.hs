c928c64a82b12ecd99a22b945859c7219ac24203
Anton Kholomiov <anton.kholomiov@gmail.com>
2013-09-21 21:09:45 +0400

    plan for tommorow. protos

--- src/Csound/Typed/Midi.hs
+++ src/Csound/Typed/Midi.hs
@@ -1,2 +1,14 @@
 module Csound.Typed.Midi where
 
+import Csound.Typed.Tuple
+import Csound.Typed.Evt
+
+data Msg = Msg
+
+type Channel = Int
+
+midi :: Out a => Channel -> (Msg -> a) -> NoSE a
+midi = undefined
+
+midiE :: Channel -> Evt Msg
+midiE = undefined
