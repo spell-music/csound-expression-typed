2460c37fc3fa2980c67c88a995996607dc31ce6e
Anton Kholomiov <anton.kholomiov@gmail.com>
2013-10-09 12:15:17 +0400

    upd for midi

--- src/Csound/Typed/Render.hs
+++ src/Csound/Typed/Render.hs
@@ -1,15 +1,18 @@
 module Csound.Typed.Render(
     renderOut, renderOutBy,
     -- * Options
-    module Csound.Typed.GlobalState.Options
+    module Csound.Typed.GlobalState.Options,
+    module Csound.Dynamic.Flags
 ) where
 
 import qualified Data.Map as M
 import Data.Default
+import Data.Maybe
 import Data.Tuple
 
 import Csound.Dynamic
 import Csound.Dynamic.Control
+import Csound.Dynamic.Flags
 import Csound.Typed.Types(Out, outArity)
 import Csound.Typed.GlobalState
 import Csound.Typed.GlobalState.Options
@@ -28,7 +31,7 @@ renderOutBy options = fmap renderCsd . (toCsd options)
 renderHistory :: Int -> Options -> History -> Csd
 renderHistory nchnls opt hist = Csd flags orc sco
     where
-        flags   = setFlags opt
+        flags   = reactOnMidi hist $ setFlags opt
         orc     = Orc (getInstr0 nchnls opt hist) (fmap (uncurry Instr) $ instrsContent $ instrs hist)
         sco     = Sco (Just $ getTotalDur opt $ totalDur hist) (renderGens $ genMap hist) [alwaysOn $ masterInstrId hist]
 
@@ -51,4 +54,13 @@ getInstr0 nchnls opt hist = do
         midiAssigns = mapM_ renderMidiAssign $ midis hist
 
         initGlobals = varsInits $ globalsVars $ globals $ hist
+
+reactOnMidi :: History -> Flags -> Flags
+reactOnMidi h flags
+    | midiIsActive h && midiDeviceIsEmpty flags = setMidiDevice flags
+    | otherwise                                 = flags
+    where
+        midiIsActive = not . null . midis
+        midiDeviceIsEmpty = isNothing . midiDevice . midiRT
+        setMidiDevice x = x { midiRT = (midiRT x) { midiDevice = Just "a" } }
 
