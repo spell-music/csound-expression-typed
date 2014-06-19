02ac12a21d6eca57d9999636c3dabc6259f73735
Anton Kholomiov <anton.kholomiov@gmail.com>
2014-06-19 16:51:23 +0400

    bugfix: set inits for opcodes with multiple outputs was wrong

--- src/Csound/Typed/Types.hs
+++ src/Csound/Typed/Types.hs
@@ -13,7 +13,6 @@ module Csound.Typed.Types(
     module Csound.Typed.Types.Evt
 ) where
 
-import Control.Applicative
 import qualified Csound.Dynamic as D
 
 import Csound.Typed.Types.Prim
@@ -28,31 +27,31 @@ import Csound.Typed.GlobalState(evalSE, SE)
 -- | Appends initialisation arguments. It's up to user to supply arguments with the right types. For example:
 --
 -- > oscil 0.5 440 sinWave `withInits` (0.5 :: D)
-withInits :: (Val a, Tuple b) => a -> b -> a
+withInits :: (Tuple a, Tuple b) => a -> b -> a
 withInits a b = genWithInits a (fromTuple b)
 
 -- | A special case of @withInits@. Here all inits are numbers. 
-withDs :: Val a => a -> [D] -> a
+withDs :: Tuple a => a -> [D] -> a
 withDs a ds = genWithInits a (mapM toGE ds)
 
 -- | Appends an init value which is a number.
-withD :: Val a => a -> D -> a
+withD :: Tuple a => a -> D -> a
 withD = withInits
 
 -- | A special case of @withInits@. Here all inits are signals. 
-withSigs :: Val a => a -> [Sig] -> a
+withSigs :: Tuple a => a -> [Sig] -> a
 withSigs a sigs = genWithInits a (mapM toGE sigs)
 
 -- | Appends an init value which is a signal.
-withSig :: Val a => a -> Sig -> a
+withSig :: Tuple a => a -> Sig -> a
 withSig = withInits
 
 -- | A special case of @withInits@. Here all inits are arrays. 
-withTabs :: Val a => a -> [Tab] -> a
+withTabs :: Tuple a => a -> [Tab] -> a
 withTabs a tabs = genWithInits a (mapM toGE tabs)
 
 -- | Appends an init value which is a table.
-withTab :: Val a => a -> Tab -> a
+withTab :: Tuple a => a -> Tab -> a
 withTab = withInits
 
 -- | Applies a seed to the random value. 
@@ -63,9 +62,9 @@ withTab = withInits
 withSeed :: SE Sig -> D -> Sig
 withSeed a d = hideGE $ evalSE $ fmap (flip withD d) a
 
-genWithInits :: (Val a) => a -> GE [E] -> a
-genWithInits a vals = fromGE $ liftA2 D.withInits (toGE a) vals
-
-
-
+genWithInits :: (Tuple a) => a -> GE [E] -> a
+genWithInits a vals = toTuple $ do
+    as <- fromTuple a
+    vs <- vals
+    return $ fmap (\x -> D.withInits x vs) as
 
