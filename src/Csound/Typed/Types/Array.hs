76ed7ea0ae64a07b28a4fe17cc8bf20b9333dd24
anton-k <anton.kholomiov@gmail.com>
2016-12-09 23:42:27 +0300

    arrays: proceed

--- src/Csound/Typed/Types/Array.hs
+++ src/Csound/Typed/Types/Array.hs
@@ -1,8 +1,11 @@
 {-# Language FlexibleInstances #-}
 module Csound.Typed.Types.Array(
     Arr(..), 
-    newLocalArr, newGlobalArr, 
-    readArr, writeArr, modifyArr, mixArr
+    newLocalArr, newGlobalArr, newLocalCtrlArr, newGlobalCtrlArr,
+    readArr, writeArr, modifyArr, mixArr,
+    -- * Misc functions to help the type inverence
+    Arr1, DArr1, Arr2, DArr2, Arr3, DArr3,
+    arr1, darr1, arr2, darr2, arr3, darr3
 ) where
 
 
@@ -17,6 +20,33 @@ import Csound.Typed.GlobalState.SE
 import Csound.Typed.GlobalState.GE
 
 import qualified Csound.Dynamic as D
+
+type Arr1 a  = Arr Sig a
+type DArr1 a = Arr D   a
+
+type Arr2 a  = Arr (Sig, Sig) a
+type DArr2 a = Arr (D, D) a
+
+type Arr3 a  = Arr (Sig, Sig, Sig) a
+type DArr3 a = Arr (D, D, D) a
+
+arr1  :: SE (Arr Sig a) -> SE (Arr Sig a)
+arr1 = id
+
+darr1 :: SE (Arr D   a) -> SE (Arr D   a)
+darr1 = id
+
+arr2  :: SE (Arr (Sig,Sig) a) -> SE (Arr (Sig,Sig) a)
+arr2 = id
+
+darr2 :: SE (Arr (D,D)   a)   -> SE (Arr (D,D)     a)
+darr2 = id
+
+arr3  :: SE (Arr (Sig,Sig,Sig) a) -> SE (Arr (Sig,Sig,Sig) a)
+arr3 = id
+
+darr3 :: SE (Arr (D,D,D)   a)     -> SE (Arr (D,D,D)     a)
+darr3 = id
 
 -- | Arrays
 newtype Arr ix a = Arr { unArr :: [Var] }
@@ -33,6 +63,16 @@ newLocalArr = newArrBy newLocalArrVar
 newGlobalArr :: Tuple a => [D] -> [a] -> SE (Arr ix a)
 newGlobalArr = newArrBy newGlobalArrVar
 
+newLocalCtrlArr :: Tuple a => [D] -> [a] -> SE (Arr ix a)
+newLocalCtrlArr = newArrBy $ newLocalArrVar . toCtrlRate
+
+newGlobalCtrlArr :: Tuple a => [D] -> [a] -> SE (Arr ix a)
+newGlobalCtrlArr = newArrBy $ newGlobalArrVar . toCtrlRate
+
+toCtrlRate x = case x of 
+    Ar -> Kr
+    Kr -> Ir
+    _  -> x
 
 readArr :: (Tuple a, Tuple ix) => Arr ix a -> ix -> SE a
 readArr (Arr vars) ixs = fmap (toTuple . return) $ SE $ hideGEinDep $ do
