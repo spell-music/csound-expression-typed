add11ba84b0f29ddbfcdcd8e9305285a55022d59
anton-k <anton.kholomiov@gmail.com>
2017-06-29 19:48:44 +0300

    move to wrapped widgets

--- src/Csound/Typed/Gui/Widget.hs
+++ src/Csound/Typed/Gui/Widget.hs
@@ -1,22 +1,23 @@
+{-# Language DeriveFunctor #-}
 module Csound.Typed.Gui.Widget(
     -- * Panels
-    panel, keyPanel, tabs, keyTabs, panels, 
+    panel, keyPanel, tabs, keyTabs, panels,
     keyPanels, panelBy, keyPanelBy, tabsBy, keyTabsBy,
 
     -- * Types
-    Input, Output, Inner,
+    Input(..), Output(..), Inner(..),
     noInput, noOutput, noInner,
-    Widget, widget, Source, source, Sink, sink, Display, display, SinkSource, sinkSource, sourceSlice, sinkSlice,
+    Widget, widget, Source(..), source, Sink(..), sink, Display(..), display, SinkSource(..), sinkSource, sourceSlice, sinkSlice,
     mapSource, mapGuiSource, mhor, mver, msca,
 
     -- * Widgets
     count, countSig, joy, knob, roller, slider, sliderBank, numeric, meter, box,
     button, butBank, butBankSig, butBank1, butBankSig1, toggle, toggleSig,
-    setNumeric, 
+    setNumeric,
     setToggle, setToggleSig, setKnob, setSlider,
     -- * Transformers
     setTitle,
-    -- * Keyboard    
+    -- * Keyboard
     KeyEvt(..), Key(..), keyIn
 ) where
 
@@ -108,7 +109,7 @@ type Inner    = SE ()
 
 -- | A value for widgets that consume nothing.
 noOutput :: Output ()
-noOutput = return 
+noOutput = return
 
 -- | A value for widgets that produce nothing.
 noInput :: Input ()
@@ -118,33 +119,34 @@ noInput  = ()
 noInner :: Inner
 noInner = return ()
 
--- | A widget consists of visible element (Gui), value consumer (Output) 
+-- | A widget consists of visible element (Gui), value consumer (Output)
 -- and producer (Input) and an inner state (Inner).
 type Widget a b = SE (Gui, Output a, Input b, Inner)
 
 -- | A consumer of the values.
-type Sink   a = SE (Gui, Output a)
+newtype Sink   a = Sink { unSink :: SE (Gui, Output a) }
 
 -- | A producer of the values.
-type Source a = SE (Gui, Input a)
+newtype Source a = Source { unSource :: SE (Gui, Input a) }
+    deriving (Functor)
 
-type SinkSource a = SE (Gui, Output a, Input a)
+newtype SinkSource a = SinkSource { unSinkSource :: SE (Gui, Output a, Input a) }
 
 -- | A static element. We can only look at it.
-type Display  = SE Gui
+newtype Display  = Display { unDisplay :: SE Gui }
 
 
 -- | A handy function for transforming the value of producers.
 mapSource :: (a -> b) -> Source a -> Source b
-mapSource f = fmap $ \(gui, ins) -> (gui, f ins) 
+mapSource = fmap
 
 -- | A handy function for transforming the GUIs of producers.
 mapGuiSource :: (Gui -> Gui) -> Source a -> Source a
-mapGuiSource f = fmap $ \(gui, ins) -> (f gui, ins) 
+mapGuiSource f (Source x) = Source $ fmap (\(gui, ins) -> (f gui, ins)) x
 
 mGroup :: Monoid a => ([Gui] -> Gui) -> [Source a] -> Source a
-mGroup guiGroup as = do
-    (gs, fs) <- fmap unzip $ sequence as    
+mGroup guiGroup as = Source $ do
+    (gs, fs) <- fmap unzip $ sequence $ fmap unSource as
     return (guiGroup gs, mconcat fs)
 
 -- | Horizontal grouping of widgets that can produce monoidal values.
@@ -164,72 +166,72 @@ widget :: SE (Gui, Output a, Input b, Inner) -> Widget a b
 widget x = go =<< x
     where
         go :: (Gui, Output a, Input b, Inner) -> Widget a b
-        go (gui, outs, ins, inner) = geToSe $ do     
+        go (gui, outs, ins, inner) = geToSe $ do
             handle <- newGuiHandle
             appendToGui (GuiNode gui handle) (unSE inner)
             return (fromGuiHandle handle, outs, ins, inner)
 
 -- | A producer constructor.
 source :: SE (Gui, Input a) -> Source a
-source x = fmap select $ widget $ fmap append x
-    where 
+source x = Source $ fmap select $ widget $ fmap append x
+    where
         select (g, _, i, _) = (g, i)
         append (g, i) = (g, noOutput, i, noInner)
 
 -- | A consumer constructor.
 sink :: SE (Gui, Output a) -> Sink a
-sink x = fmap select $ widget $ fmap append x
-    where 
+sink x = Sink $ fmap select $ widget $ fmap append x
+    where
         select (g, o, _, _) = (g, o)
         append (g, o) = (g, o, noInput, noInner)
 
 sinkSource :: SE (Gui, Output a, Input a) -> SinkSource a
-sinkSource x = fmap select $ widget $ fmap append x
+sinkSource x = SinkSource $ fmap select $ widget $ fmap append x
     where
         select (g, o, i, _) = (g, o, i)
         append (g, o, i) = (g, o, i, noInner)
 
 -- | A display constructor.
-display :: SE Gui -> Display 
-display x = fmap select $ widget $ fmap append x
-    where 
+display :: SE Gui -> Display
+display x = Display $ fmap select $ widget $ fmap append x
+    where
         select (g, _, _, _) = g
-        append g = (g, noOutput, noInput, noInner)        
+        append g = (g, noOutput, noInput, noInner)
 
------------------------------------------------------------------------------  
+-----------------------------------------------------------------------------
 -- primitive elements
 
 -- | Appends a title to a group of widgets.
 setTitle :: String -> Gui -> SE Gui
-setTitle name g 
+setTitle name g
     | null name = return g
     | otherwise = do
-        gTitle <- box name
+        gTitle <- unDisplay $ box name
         return $ ver [sca 0.01 gTitle, g]
 
 setSourceTitle :: String -> Source a -> Source a
-setSourceTitle name ma = source $ do
+setSourceTitle name (Source ma) = source $ do
     (gui, val) <- ma
     newGui <- setTitle name gui
     return (newGui, val)
 
 setLabelSource :: String -> Source a -> Source a
-setLabelSource a 
+setLabelSource a
     | null a    = id
-    | otherwise = fmap (first $ setLabel a)
+    | otherwise = Source . fmap (first $ setLabel a) . unSource
 
 setLabelSink :: String -> Sink a -> Sink a
-setLabelSink a 
+setLabelSink a
     | null a    = id
-    | otherwise = fmap (first $ setLabel a)
+    | otherwise = Sink . fmap (first $ setLabel a) . unSink
 
 setLabelSnkSource :: String -> SinkSource a -> SinkSource a
-setLabelSnkSource a 
+setLabelSnkSource a
     | null a    = id
-    | otherwise = fmap (\(x, y, z) -> (setLabel a x, y, z)) 
+    | otherwise = SinkSource . fmap (\(x, y, z) -> (setLabel a x, y, z)) . unSinkSource
 
-singleOut :: Maybe Double -> Elem -> Source Sig 
-singleOut v0 el = geToSe $ do
+singleOut :: Maybe Double -> Elem -> Source Sig
+singleOut v0 el = Source $ geToSe $ do
     (var, handle) <- newGuiVar
     let handleVar = guiHandleToVar handle
         inits = maybe [] (return . InitMe handleVar) v0
@@ -237,17 +239,17 @@ singleOut v0 el = geToSe $ do
     appendToGui (GuiNode gui handle) (unSE noInner)
     return (fromGuiHandle handle, readSig var)
 
-singleIn :: (GuiHandle -> Output Sig) -> Maybe Double -> Elem -> Sink Sig 
-singleIn outs v0 el = geToSe $ do
+singleIn :: (GuiHandle -> Output Sig) -> Maybe Double -> Elem -> Sink Sig
+singleIn outs v0 el = Sink $ geToSe $ do
     (var, handle) <- newGuiVar
-    let handleVar = guiHandleToVar handle        
+    let handleVar = guiHandleToVar handle
         inits = maybe [] (return . InitMe handleVar) v0
         gui = fromElem [var, handleVar] inits el
     appendToGui (GuiNode gui handle) (unSE noInner)
     return (fromGuiHandle handle, outs handle)
 
 singleInOut :: (GuiHandle -> Output Sig) -> Maybe Double -> Elem -> SinkSource Sig
-singleInOut outs v0 el = geToSe $ do
+singleInOut outs v0 el = SinkSource $ geToSe $ do
     (var, handle) <- newGuiVar
     let handleVar = guiHandleToVar handle
         inits = maybe [] (return . InitMe handleVar) v0
@@ -255,29 +257,29 @@ singleInOut outs v0 el = geToSe $ do
     appendToGui (GuiNode gui handle) (unSE noInner)
     return (fromGuiHandle handle, outs handle, readSig var)
 
--- | A variance on the function 'Csound.Gui.Widget.count', but it produces 
--- a signal of piecewise constant function. 
+-- | A variance on the function 'Csound.Gui.Widget.count', but it produces
+-- a signal of piecewise constant function.
 countSig :: ValDiap -> ValStep -> Maybe ValStep -> Double -> Source Sig
 countSig diap step1 mValStep2 v0 = singleOut (Just v0) $ Count diap step1 mValStep2
 
--- | Allows the user to increase/decrease a value with mouse 
--- clicks on a corresponding arrow button. Output is an event stream that contains 
+-- | Allows the user to increase/decrease a value with mouse
+-- clicks on a corresponding arrow button. Output is an event stream that contains
 -- values when counter changes.
--- 
--- > count diapason fineValStep maybeCoarseValStep initValue 
--- 
+--
+-- > count diapason fineValStep maybeCoarseValStep initValue
+--
 -- doc: http://www.csounds.com/manual/html/FLcount.html
 count :: ValDiap -> ValStep -> Maybe ValStep -> Double -> Source (Evt D)
 count diap step1 mValStep2 v0 = mapSource snaps $ countSig diap step1 mValStep2 v0
 
--- | It is a squared area that allows the user to modify two output values 
--- at the same time. It acts like a joystick. 
--- 
--- > joy valueSpanX valueSpanY (initX, initY) 
+-- | It is a squared area that allows the user to modify two output values
+-- at the same time. It acts like a joystick.
+--
+-- > joy valueSpanX valueSpanY (initX, initY)
 --
 -- doc: <http://www.csounds.com/manual/html/FLjoy.html>
 joy :: ValSpan -> ValSpan -> (Double, Double) -> Source (Sig, Sig)
-joy sp1 sp2 (x, y) = geToSe $ do
+joy sp1 sp2 (x, y) = Source $ geToSe $ do
     (var1, handle1) <- newGuiVar
     (var2, handle2) <- newGuiVar
     let handleVar1 = guiHandleToVar handle1
@@ -296,7 +298,7 @@ joy sp1 sp2 (x, y) = geToSe $ do
 knob :: String -> ValSpan -> Double -> Source Sig
 knob name sp v0 = setLabelSource name $ singleOut (Just v0) $ Knob sp
 
--- | FLroller is a sort of knob, but put transversally. 
+-- | FLroller is a sort of knob, but put transversally.
 --
 -- > roller valueSpan step initVal
 --
@@ -306,7 +308,7 @@ roller name sp step v0 = setLabelSource name $ singleOut (Just v0) $ Roller sp s
 
 -- | FLslider puts a slider into the corresponding container.
 --
--- > slider valueSpan initVal 
+-- > slider valueSpan initVal
 --
 -- doc: <http://www.csounds.com/manual/html/FLslider.html>
 slider :: String -> ValSpan -> Double -> Source Sig
@@ -316,18 +318,18 @@ slider name sp v0 = setLabelSource name $ singleOut (Just v0) $ Slider sp
 -- of init values.
 sliderBank :: String -> [Double] -> Source [Sig]
 sliderBank name ds = source $ do
-    (gs, vs) <- fmap unzip $ zipWithM (\n d -> slider (show n) uspan d) [(1::Int) ..] ds 
+    (gs, vs) <- fmap unzip $ zipWithM (\n d -> unSource $ slider (show n) uspan d) [(1::Int) ..] ds
     gui <- setTitle name  $ hor gs
     return (gui, vs)
 
--- | numeric (originally FLtext in the Csound) allows the user to modify 
+-- | numeric (originally FLtext in the Csound) allows the user to modify
 -- a parameter value by directly typing it into a text field.
 --
--- > numeric diapason step initValue 
+-- > numeric diapason step initValue
 --
 -- doc: <http://www.csounds.com/manual/html/FLtext.html>
 numeric :: String -> ValDiap -> ValStep -> Double -> Source Sig
-numeric name diap step v0 = setLabelSource name $ singleOut (Just v0) $ Text diap step 
+numeric name diap step v0 = setLabelSource name $ singleOut (Just v0) $ Text diap step
 
 -- | A FLTK widget that displays text inside of a box.
 -- If the text is longer than 255 characters the text
@@ -337,59 +339,59 @@ numeric name diap step v0 = setLabelSource name $ singleOut (Just v0) $ Text dia
 --
 -- doc: <http://www.csounds.com/manual/html/FLbox.html>
 box :: String -> Display
-box label 
+box label
     | length label < lim = rawBox label
-    | otherwise          = fmap (padding 0 . ver) $ mapM rawBox $ parts lim label
-    where 
-        parts n xs 
+    | otherwise          = Display $ fmap (padding 0 . ver) $ mapM (unDisplay . rawBox) $ parts lim label
+    where
+        parts n xs
             | length xs < n = [xs]
             | otherwise     = a : parts n b
             where (a, b) = splitAt n xs
         lim = 255
 
 rawBox :: String -> Display
-rawBox label = geToSe $ do
+rawBox label = Display $ geToSe $ do
     (_, handle) <- newGuiVar
     let gui = fromElem [guiHandleToVar handle] [] (Box label)
     appendToGui (GuiNode gui handle) (unSE noInner)
     return $ fromGuiHandle handle
 
--- | A FLTK widget opcode that creates a button. 
+-- | A FLTK widget opcode that creates a button.
 --
 -- > button text
--- 
+--
 -- doc: <http://www.csounds.com/manual/html/FLbutton.html>
 button :: String -> Source (Evt Unit)
 button name = setLabelSource name $ source $ do
     flag <- geToSe $ onGlobals $ C.newPersistentGlobalVar Kr 0
-    flagChanged <- geToSe $ onGlobals $ C.newPersistentGlobalVar Kr 0    
+    flagChanged <- geToSe $ onGlobals $ C.newPersistentGlobalVar Kr 0
     instrId <- geToSe $ saveInstr $ instr flag
     geToSe $ (saveAlwaysOnInstr =<< ) $ saveInstr $ instrCh flag flagChanged
-    (g, _) <- singleOut Nothing (Button instrId)
+    (g, _) <- unSource $ singleOut Nothing (Button instrId)
     val <- fmap fromGE $ fromDep $ readVar flagChanged
     return (g, sigToEvt val)
     where
         instr ref = SE $ do
             val <- readVar ref
-            whens Kr 
+            whens Kr
                 [ (val ==* 0, writeVar ref 1)
-                ] (writeVar ref 0)            
+                ] (writeVar ref 0)
             turnoff
 
         instrCh ref refCh = SE $ do
             val <- readVar ref
-            writeVar refCh (C.changed val)        
-            
+            writeVar refCh (C.changed val)
+
 -- | A FLTK widget opcode that creates a toggle button.
 --
 -- > button text
--- 
+--
 -- doc: <http://www.csounds.com/manual/html/FLbutton.html>
 toggle :: String -> Bool -> Source (Evt D)
 toggle name initVal = mapSource snaps $ toggleSig name initVal
-    
--- | A variance on the function 'Csound.Gui.Widget.toggle', but it produces 
--- a signal of piecewise constant function. 
+
+-- | A variance on the function 'Csound.Gui.Widget.toggle', but it produces
+-- a signal of piecewise constant function.
 toggleSig :: String -> Bool -> Source Sig
 toggleSig name initVal = setLabelSource name $ singleOut (initToggle initVal) Toggle
 
@@ -398,9 +400,9 @@ initToggle a = if a then (Just 1) else Nothing
 
 -- | A FLTK widget opcode that creates a bank of buttons.
 -- Result is (x, y) coordinate of the triggered button.
--- 
+--
 -- > butBank xNumOfButtons yNumOfButtons
--- 
+--
 -- doc: <http://www.csounds.com/manual/html/FLbutBank.html>
 butBank :: String -> Int -> Int -> (Int, Int) -> Source (Evt (D, D))
 butBank name xn yn inits = mapSource (fmap split2 . snaps) $ butBankSig1 name xn yn inits
@@ -409,31 +411,31 @@ butBank name xn yn inits = mapSource (fmap split2 . snaps) $ butBankSig1 name xn
         x = int xn
         y = int yn
 
--- | A variance on the function 'Csound.Gui.Widget.butBank', but it produces 
--- a signal of piecewise constant function. 
+-- | A variance on the function 'Csound.Gui.Widget.butBank', but it produces
+-- a signal of piecewise constant function.
 -- Result is (x, y) coordinate of the triggered button.
 butBankSig :: String -> Int -> Int -> (Int, Int) -> Source (Sig, Sig)
 butBankSig name xn yn inits = mapSource split2 $ butBankSig1 name xn yn inits
-    where 
+    where
         split2 a = (floor' $ a / y, mod' a x)
         x = sig $ int xn
         y = sig $ int yn
 
 -- | A FLTK widget opcode that creates a bank of buttons.
--- 
+--
 -- > butBank xNumOfButtons yNumOfButtons
--- 
+--
 -- doc: <http://www.csounds.com/manual/html/FLbutBank.html>
 butBank1 :: String -> Int -> Int -> (Int, Int) -> Source (Evt D)
 butBank1 name xn yn inits = mapSource snaps $ butBankSig1 name xn yn inits
-        
-butBankSig1 :: String -> Int -> Int -> (Int, Int) -> Source Sig 
+
+butBankSig1 :: String -> Int -> Int -> (Int, Int) -> Source Sig
 butBankSig1 name xn yn (x0, y0) = setSourceTitle name $ singleOut (Just n) $ ButBank xn yn
     where n = fromIntegral $ y0 + x0 * yn
 
 -- |  FLtext that is sink shows current the value of a valuator in a text field.
 setNumeric :: String -> ValDiap -> ValStep -> Double -> Sink Sig
-setNumeric name diap step v0 = setLabelSource name $ singleIn printk2 (Just v0) $ Text diap step 
+setNumeric name diap step v0 = setLabelSink name $ singleIn printk2 (Just v0) $ Text diap step
 
 -- | A slider that serves as indicator. It consumes values instead of producing.
 --
@@ -449,7 +451,7 @@ setToggleSig name initVal = setLabelSnkSource name $ singleInOut setVal (initTog
 
 setToggle :: String -> Bool -> SinkSource (Evt D)
 setToggle name initVal = sinkSource $ do
-    (g, outs, ins) <- setToggleSig name initVal
+    (g, outs, ins) <- unSinkSource $ setToggleSig name initVal
     let evtOuts a = outs =<< stepper 0 (fmap sig a)
     return (g, evtOuts, snaps ins)
 
@@ -464,16 +466,16 @@ setSlider name sp v0 = setLabelSnkSource name $ singleInOut setVal' (Just v0) $ 
 
 -- | The stream of keyboard press/release events.
 keyIn :: KeyEvt -> Evt Unit
-keyIn evt = boolToEvt $ asig ==* 1    
+keyIn evt = boolToEvt $ asig ==* 1
     where asig = Sig $ fmap readOnlyVar $ listenKeyEvt evt
 
 -- Outputs
 
 readD :: Var -> SE D
-readD v = fmap (D . return) $ SE $ readVar v 
+readD v = fmap (D . return) $ SE $ readVar v
 
 readSig :: Var -> Sig
-readSig v = Sig $ return $ readOnlyVar v 
+readSig v = Sig $ return $ readOnlyVar v
 
 
 refHandle :: GuiHandle -> SE D
@@ -503,8 +505,8 @@ flPrintk2 val handle = SE $ (depT_ =<<) $ lift $ f <$> toGE val <*> toGE handle
 -----------------------------------------------------
 
 sourceSlice :: SinkSource a -> Source a
-sourceSlice = fmap (\(gui, _, a) -> (gui, a))
+sourceSlice = Source . (fmap (\(gui, _, a) -> (gui, a))) . unSinkSource
 
 sinkSlice :: SinkSource a -> Sink a
-sinkSlice = fmap (\(gui, a, _) -> (gui, a))
+sinkSlice = Sink . (fmap (\(gui, a, _) -> (gui, a))) . unSinkSource
 
